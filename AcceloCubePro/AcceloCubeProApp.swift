4c8a5e9a10c1ec2dffff4f1073c8e1311f0b3671
DS4506 <davidsmith4506@gmail.com>
2025-10-15 00:40:57 -0500

    Initial Commit

---
+++ AcceloCubePro/AcceloCubeProApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcceloCubeProApp.swift
+//  AcceloCubePro
+//
+//  Created by Willie Earl on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct AcceloCubeProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
